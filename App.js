8732319dd06541122e04cf2cb37f7b4b31f8d018
robertocastillo <robertocastillo945@gmail.com>
2021-08-21 14:41:28 -0600

    final

--- App.js
+++ App.js
@@ -1,21 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View,TextInput ,Button} from 'react-native';
 
 export default function App() {
+
+  const [text, setText] = useState('React Native')
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.header}>
+        <Text style={styles.title}>{text}</Text>
+      </View>
+
+      <View style={styles.body}>
+
+          <View style={styles.main}>
+            <TextInput
+              
+              onChangeText={text => setText(text)} 
+              style={styles.input} 
+              placeholder="Escriba aqui..."/>
+            <Button
+            title="Click me"/>
+
+          </View>
+         
+
+      </View>
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   flex:1,
+   flexDirection:'column',
+   alignItems:'center',
+   backgroundColor:'#dfe6e9',
+   justifyContent:'space-evenly'
+  
   },
+  header:{
+    marginTop:20
+  },
+  title:{
+    fontSize:30,
+    fontWeight:'bold'
+  },
+  main:{
+    flexDirection:'row',
+    
+  },
+  input:{
+    padding:5,
+    height:40,
+    width:'70%',
+    backgroundColor:'white',
+    borderRadius:5,
+    marginRight:20
+  },
+  btn:{
+    marginTop:20,
+    width:'100px'
+  },
+ 
 });
